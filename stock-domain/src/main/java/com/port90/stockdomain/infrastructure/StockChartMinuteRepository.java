888a800460ec6237b2bd733b98de60e3d36f71b5
hyoguoo <hyoguoo@gmail.com>
2024-12-02 12:52:01 +0900

    feat: 일봉 / 분봉을 통해 통계를 내기 위한 데이터 조회 메서드 구현

--- stock-domain/src/main/java/com/port90/stockdomain/infrastructure/StockChartMinuteRepository.java
+++ stock-domain/src/main/java/com/port90/stockdomain/infrastructure/StockChartMinuteRepository.java
@@ -2,9 +2,30 @@ package com.port90.stockdomain.infrastructure;
 
 import com.port90.stockdomain.domain.chart.StockChartMinute;
 import com.port90.stockdomain.domain.chart.StockChartMinuteId;
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface StockChartMinuteRepository extends JpaRepository<StockChartMinute, StockChartMinuteId> {
+
+    // TODO: 분봉 데이터 정확한 계산 방법 확인 필요
+    @Query("""
+            SELECT m FROM StockChartMinute m
+            WHERE m.stockCode = :stockCode
+            AND m.date = :date
+            AND :startTime < m.time
+            AND m.time <= :endTime
+            ORDER BY m.time ASC
+            """)
+    List<StockChartMinute> findByStockCodeAndDateAndTimeBetween(
+            @Param("stockCode") String stockCode,
+            @Param("date") LocalDate date,
+            @Param("startTime") LocalTime startTime,
+            @Param("endTime") LocalTime endTime
+    );
 }
