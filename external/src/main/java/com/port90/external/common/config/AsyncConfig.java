ab5d5b62a7acad127d6ad95035e81e10b06a3a34
cykei <kelle111@naver.com>
2024-12-04 15:07:11 +0900

    [FIX] 비동기 테스트

--- external/src/main/java/com/port90/external/common/config/AsyncConfig.java
+++ external/src/main/java/com/port90/external/common/config/AsyncConfig.java
@@ -11,9 +11,9 @@ public class AsyncConfig {
    @Bean
     public ThreadPoolTaskExecutor taskExecutor() {
         ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
-        executor.setCorePoolSize(10); // 코어 스레드 풀 크기 설정
-        executor.setMaxPoolSize(20); // 최대 스레드 풀 크기 설정
-        executor.setQueueCapacity(100); // 작업 큐 용량 설정
+        executor.setCorePoolSize(4); // 코어 스레드 풀 크기 설정
+        executor.setMaxPoolSize(10); // 최대 스레드 풀 크기 설정
+        executor.setQueueCapacity(500); // 작업 큐 용량 설정
         executor.setThreadNamePrefix("stock-"); // 스레드 이름 접두사 설정
 
         // 작업이 완료된 후 스레드 풀이 종료될 때까지 대기할 시간 설정 (단위: 초)
