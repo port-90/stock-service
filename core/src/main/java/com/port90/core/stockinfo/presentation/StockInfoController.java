510c23eee21a57930da1b13f99e563b9d2f15293
LSG <sskkilm@naver.com>
2025-01-13 17:15:47 +0900

    feat: 종목 코드 또는 종목 이름으로 종목 정보 검색 기능 구현

---
+++ core/src/main/java/com/port90/core/stockinfo/presentation/StockInfoController.java
@@ -0,0 +1,29 @@
+package com.port90.core.stockinfo.presentation;
+
+import com.port90.core.stockinfo.application.StockInfoService;
+import com.port90.core.stockinfo.dto.StockInfoDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/stock-infos")
+public class StockInfoController {
+
+    private final StockInfoService stockInfoService;
+
+    @GetMapping
+    public List<StockInfoDto> getStockInfosByCondition(
+            @RequestParam(required = false) String stockCode,
+            @RequestParam(required = false) String stockName,
+            @RequestParam(required = false) String cursor,
+            @RequestParam(defaultValue = "10") int size
+    ) {
+        return stockInfoService.getStockInfosByCondition(stockCode, stockName, cursor, size);
+    }
+}
